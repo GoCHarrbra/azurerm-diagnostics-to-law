f459963b69e955f65216d3f84ba909bcf4d7a59f
GoCHarrbra <brad.harrison@ecn.forces.gc.ca>
2025-11-21 08:31:47 -0500

    No more ternary operators

--- main.tf
+++ main.tf
@@ -1,31 +1,12 @@
-data "azurerm_monitor_diagnostic_categories" "this" {
-  resource_id = var.target_resource_id
-}
-
-locals {
-  # New schema in azurerm v4.x:
-  # - log_category_types: list of log category names
-  # - metrics: list of metric category names
-  logs_all    = toset(data.azurerm_monitor_diagnostic_categories.this.log_category_types)
-  metrics_all = toset(data.azurerm_monitor_diagnostic_categories.this.metrics)
-
-  # If caller passes an empty list, we fall back to "all" for that type.
-  logs_effective = length(var.include_log_categories) > 0
-    ? toset(var.include_log_categories)
-    : local.logs_all
-
-  metrics_effective = length(var.include_metric_categories) > 0
-    ? toset(var.include_metric_categories)
-    : local.metrics_all
-}
-
 resource "azurerm_monitor_diagnostic_setting" "this" {
   name                       = var.name
   target_resource_id         = var.target_resource_id
   log_analytics_workspace_id = var.law_resource_id
 
+  # Logs – caller passes the exact category names.
+  # If the list is empty, no log blocks are created.
   dynamic "enabled_log" {
-    for_each = local.logs_effective
+    for_each = toset(var.include_log_categories)
     content {
       category = enabled_log.value
 
@@ -35,8 +16,9 @@ resource "azurerm_monitor_diagnostic_setting" "this" {
     }
   }
 
+  # Metrics – same idea, caller passes category names (e.g. "AllMetrics").
   dynamic "metric" {
-    for_each = local.metrics_effective
+    for_each = toset(var.include_metric_categories)
     content {
       category = metric.value
 
