17bf9f4f6d94354dcff65d0a1038760b3cb07635
GoCHarrbra <brad.harrison@ecn.forces.gc.ca>
2025-11-21 09:07:45 -0500

    remove deprecated warning

--- main.tf
+++ main.tf
@@ -1,3 +1,30 @@
+variable "name" {
+  description = "Name of the diagnostic setting."
+  type        = string
+}
+
+variable "target_resource_id" {
+  description = "ARM ID of the resource to which diagnostics are attached."
+  type        = string
+}
+
+variable "law_resource_id" {
+  description = "ARM ID of the Log Analytics workspace."
+  type        = string
+}
+
+variable "include_log_categories" {
+  description = "Exact log categories to enable (e.g. [\"ContainerAppConsoleLogs\"])."
+  type        = list(string)
+  default     = []
+}
+
+variable "include_metric_categories" {
+  description = "Exact metric categories to enable (e.g. [\"AllMetrics\"])."
+  type        = list(string)
+  default     = []
+}
+
 resource "azurerm_monitor_diagnostic_setting" "this" {
   name                       = var.name
   target_resource_id         = var.target_resource_id
@@ -9,10 +36,7 @@ resource "azurerm_monitor_diagnostic_setting" "this" {
     for_each = toset(var.include_log_categories)
     content {
       category = enabled_log.value
-
-      retention_policy {
-        enabled = false
-      }
+      # retention_policy removed (deprecated)
     }
   }
 
@@ -21,10 +45,7 @@ resource "azurerm_monitor_diagnostic_setting" "this" {
     for_each = toset(var.include_metric_categories)
     content {
       category = metric.value
-
-      retention_policy {
-        enabled = false
-      }
+      # retention_policy removed (deprecated)
     }
   }
 }
