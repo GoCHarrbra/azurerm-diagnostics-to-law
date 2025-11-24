fe7f10fc6ad7947eba64b4f875096d95db3f3073
GoCHarrbra <brad.harrison@ecn.forces.gc.ca>
2025-11-24 18:06:55 -0500

    fix to cosmos dependency

--- ESLZ/cosmos_to_law/cosmos_diagnostics_to_infra_law.tf
+++ ESLZ/cosmos_to_law/cosmos_diagnostics_to_infra_law.tf
@@ -5,7 +5,7 @@ module "cosmos_diagnostics" {
   source = "github.com/GoCHarrbra/azurerm-diagnostics-to-law.git?ref=v0.6.0"
 
   name               = var.cosmos_diag.name
-  target_resource_id = module.cosmos.account_id
+  target_resource_id = module.cosmos.cosmos_account_id
   law_resource_id    = var.cosmos_diag.platform_law_id
 
   include_log_categories    = var.cosmos_diag.include_log_categories
